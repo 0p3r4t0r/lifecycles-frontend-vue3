0f95df71d96ec01654b68203beb5846e05365e52
0p3r4t0r <robert83sutherland@gmail.com>
2021-06-28 07:44:29 +0900

    Add typescript support.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <html>
-    <img alt="Vue logo" src="./assets/base/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </html>
+  <img alt="Vue logo" src="./assets/base/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import HelloWorld from './components/base/pages/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
   }
-}
+});
 </script>
 
 <style>
