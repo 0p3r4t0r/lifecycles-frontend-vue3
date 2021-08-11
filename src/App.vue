59ceb2c9d97ac60c5c8d8a3779a93a76cf115e94
0p3r4t0r <robert83sutherland@gmail.com>
2021-08-11 21:42:36 +0900

    feature: Add material icons.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <!-- https://stackoverflow.com/questions/50303454/how-to-use-the-new-material-design-icon-themes-outlined-rounded-two-tone-and -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+    rel="stylesheet"
+  />
   <Navbar :links="links" />
   <router-view />
 </template>
---
+++ src/components/base/atoms/Icon/Icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="material-icons">{{ name }}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Icon",
+  props: {
+    style: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
